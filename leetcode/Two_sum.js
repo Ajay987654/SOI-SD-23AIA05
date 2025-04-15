36edce84d5a510cb782b550a3b15ee1468b83486
Ajay S <147607241+Ajay987654@users.noreply.github.com>
2025-04-15 09:17:04 +0530

    Update Two_sum.js

--- leetcode/Two_sum.js
+++ leetcode/Two_sum.js
@@ -1,1 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map(); // stores number and its index
 
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+
+    return []; // just in case there's no solution
+};
